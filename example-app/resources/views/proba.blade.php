f0e48d7986d07b2b30b289066940615b88b92d3e
Neved <emailcimed@example.com>
2023-10-02 20:58:35 +0200

    proba commitz

---
+++ example-app/resources/views/proba.blade.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('Bootstrap\css\bootstrap.css)}}">
+    <title>Ps4</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        .container {
+            width: 80%;
+            margin: auto;
+            overflow: hidden;
+        }
+
+        #header {
+            background: #35424a;
+            color: #ffffff;
+            padding-top: 30px;
+            min-height: 70px;
+            border-bottom: #e8491d 2px solid;
+        }
+
+        #header h1 {
+            text-align: center;
+        }
+
+        #main {
+            padding: 20px 0;
+        }
+
+        .console-info {
+            margin: 20px;
+            padding: 20px;
+            background: #ffffff;
+            border: #e8491d 1px solid;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+<div id="header">
+    <div class="container">
+        <h1>PS4 Játékkonzol</h1>
+    </div>
+</div>
+
+<div id="main">
+    <div class="container">
+        <div class="console-info">
+            <h2>PlayStation 4 (PS4)</h2>
+            <p>A PlayStation 4 (PS4) egy népszerű játékkonzol, melyet a Sony Interactive Entertainment fejlesztett ki. A PS4 számos izgalmas játékkal és funkcióval rendelkezik, ideális választás a játékosok számára.</p>
+            <h3>Főbb jellemzők:</h3>
+            <ul>
+                <li>Erőteljes AMD processzor és grafikus kártya.</li>
+                <li>8 GB GDDR5 memória.</li>
+                <li>500 GB vagy 1 TB merevlemez.</li>
+                <li>Blu-ray lejátszó és online streaming lehetőségek.</li>
+                <li>Wi-Fi és Bluetooth kapcsolat.</li>
+                <li>DualShock 4 vezeték nélküli kontroller.</li>
+                <li>Kiváló játékélmény és exkluzív címek.</li>
+            </ul>
+        </div>
+    </div>
+</div>
+</body>
+</html>
